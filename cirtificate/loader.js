27b432d504f02aba0cf977fc65cd71da5fb87772
vabhi2024 <vabhi2024@gmail.com>
2025-05-12 09:17:44 +0530

    Delete cirtificate directory

--- cirtificate/loader.js
+++
@@ -1,6 +0,0 @@
-// Simulate verification process
-        setTimeout(() => {
-            document.querySelector(".preloader").style.opacity = "0";
-            setTimeout(() => document.querySelector(".preloader").style.display = "none", 500);
-        }, 3000);
-
